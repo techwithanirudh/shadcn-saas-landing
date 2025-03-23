fca8523aa2460e1ac3d7c34e6c0df474541dc133
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-03-23 17:47:08 +0000

    fix: use VERCEL_PROJECT_PRODUCTION_URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 const baseUrl =
-  process.env.NODE_ENV === 'development' || !process.env.VERCEL_URL
+  process.env.NODE_ENV === 'development' || !process.env.VERCEL_PROJECT_PRODUCTION_URL
     ? new URL('https://localhost:3000')
-    : new URL(`https://${process.env.VERCEL_URL}`);
+    : new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`);
 
 module.exports = {
   siteUrl: baseUrl,
